a1c73ecf3024a890104e1f5437f39e558aed80dd
Benito Palacios Sanchez <pleonex@protonmail.com>
2023-08-02 19:03:04 +0200

    :sparkles: [MAIU] Show sensor end and controls to select date

--- src/net/PleOps.LibreGlucoseWatcher.Maui/Pages/HomeViewModel.cs
+++ src/net/PleOps.LibreGlucoseWatcher.Maui/Pages/HomeViewModel.cs
@@ -23,8 +23,13 @@ public partial class HomeViewModel : ObservableObject, IDisposable
     private readonly LibreGlucoseClient client;
     private readonly ILogger<HomeViewModel> logger;
     private readonly Timer periodicFetchTimer;
+    private readonly RectangularSection selectedDateRegion;
 
+    private IList<GlucoseMeasurement>? currentGraphData;
     private GlucoseMeasurement? currentMeasurement;
+
+    private DateTime selectedDate;
+    private TimeSpan selectedTime;
 
     [ObservableProperty]
     private string currentGlucose = string.Empty;
@@ -62,6 +67,18 @@ public partial class HomeViewModel : ObservableObject, IDisposable
     [ObservableProperty]
     private RectangularSection[] graphSections = Array.Empty<RectangularSection>();
 
+    [ObservableProperty]
+    private string sensorEndsText = string.Empty;
+
+    [ObservableProperty]
+    private DateTime firstValueDateTime = DateTime.Now;
+
+    [ObservableProperty]
+    private DateTime lastValueDateTime = DateTime.Now;
+
+    [ObservableProperty]
+    private string selectedGlucoseText = string.Empty;
+
     public HomeViewModel(LibreGlucoseClient client, ILogger<HomeViewModel> logger)
     {
         ArgumentNullException.ThrowIfNull(client);
@@ -69,6 +86,18 @@ public partial class HomeViewModel : ObservableObject, IDisposable
 
         this.client = client;
         this.logger = logger;
+
+        var now = DateTime.Now;
+        selectedDateRegion = new RectangularSection {
+            Xi = DateTime.Now.Ticks,
+            Xj = DateTime.Now.Ticks,
+            Fill = null,
+            Stroke = new SolidColorPaint(SKColors.Blue) { StrokeThickness = 1 },
+            IsVisible = true,
+        };
+        selectedDate = DateTime.Today;
+        selectedTime = now.TimeOfDay;
+
         periodicFetchTimer = new Timer(
             async (_) => await FetchGlucose(),
             null,
@@ -76,11 +105,66 @@ public partial class HomeViewModel : ObservableObject, IDisposable
             FetchTimerMs);
     }
 
+    public DateTime SelectedDate {
+        get => selectedDate;
+        set {
+            if (SetProperty(ref selectedDate, value)) {
+                UpdateSelectedRegion();
+            }
+        }
+    }
+
+    public TimeSpan SelectedTime {
+        get => selectedTime;
+        set {
+            if (SetProperty(ref selectedTime, value)) {
+                UpdateSelectedRegion();
+            }
+        }
+    }
+
+    private void UpdateSelectedRegion()
+    {
+        long selectedTicks = (SelectedDate + selectedTime).Ticks;
+        selectedDateRegion.Xi = selectedTicks;
+        selectedDateRegion.Xj = selectedTicks;
+
+        var glucose = currentGraphData?.FirstOrDefault(m => m.UtcTimestamp.ToLocalTime().Ticks >= selectedTicks);
+        SelectedGlucoseText = glucose is null
+            ? string.Empty
+            : $"Selected: {glucose.ValueInMgPerDl} mg/dL";
+    }
+
+    [RelayCommand]
+    private void BackwardSelectedTime()
+    {
+        if (SelectedTime.TotalMinutes < 5) {
+            int diff = 5 - (int)SelectedTime.TotalMinutes;
+            SelectedDate = SelectedDate.AddDays(-1);
+            SelectedTime = new TimeSpan(23, 60 - diff, 0);
+        } else {
+            SelectedTime -= TimeSpan.FromMinutes(5);
+        }
+    }
+
+    [RelayCommand]
+    private void ForwardSelectedTime()
+    {
+        if (SelectedTime.TotalMinutes > ((24 * 60) - 5)) {
+            int diff = 5 - (int)SelectedTime.TotalMinutes;
+            SelectedDate = SelectedDate.AddDays(1);
+            SelectedTime = new TimeSpan(0, diff, 0);
+        } else {
+            SelectedTime += TimeSpan.FromMinutes(5);
+        }
+    }
+
     [RelayCommand]
     private async Task FetchGlucose()
     {
         try
         {
+            // TODO: wow, this needs a good refactor!
             string patId = UserSettings.GetPatientId()!;
 
             var previous = currentMeasurement;
@@ -90,40 +174,62 @@ public partial class HomeViewModel : ObservableObject, IDisposable
             var patientInfo = graphResult.Data.Patient;
             PatientName = $"{patientInfo.FirstName} {patientInfo.LastName}";
 
-            CurrentGlucose = currentMeasurement.ValueInMgPerDl.ToString();
-            CurrentTrend = TrendToUnicode(currentMeasurement.TrendArrow);
-            CurrentGlucoseColor = MeasurementColorToColor(currentMeasurement.MeasurementColor);
-
             var timestamp = currentMeasurement.UtcTimestamp;
             var now = DateTime.UtcNow;
             var diff = now - timestamp;
+
+            CurrentGlucose = currentMeasurement.ValueInMgPerDl.ToString();
+            CurrentTrend = TrendToUnicode(currentMeasurement.TrendArrow);
+
+            var color = (diff.TotalMinutes > 15) ? MeasurementColor.Unknown : currentMeasurement.MeasurementColor;
+            CurrentGlucoseColor = MeasurementColorToColor(color);
+
             MeasurementTime = (diff.TotalHours < 24)
                 ? timestamp.ToLocalTime().ToShortTimeString()
                 : $"{timestamp.ToLocalTime().ToShortDateString()} {timestamp.ToLocalTime().ToShortTimeString()}";
 
-            if (previous is not null && previous.Timestamp != currentMeasurement.Timestamp)
+            FetchTime = $"Last check {now.ToLocalTime().ToShortTimeString()}";
+
+            var sensorEnd = patientInfo.Sensor!.StartDate + TimeSpan.FromDays(14);
+            var sensorRem = sensorEnd - DateTimeOffset.UtcNow;
+            string sensorRemText = (sensorRem.TotalHours > 24)
+                ? $"{(int)sensorRem.TotalDays} days"
+                : $"{(int)sensorRem.TotalHours} hours";
+
+            string sensorEndDateText = sensorEnd.ToLocalTime().ToString("dd.MM HH:mm");
+            SensorEndsText = $"Sensor ends in {sensorRemText} @ {sensorEndDateText}";
+
+            bool hasChanged = previous is not null && previous.Timestamp != currentMeasurement.Timestamp;
+            bool isFirst = previous is null;
+            if (hasChanged)
             {
-                PreviousMeasurementOverview = $"{previous.ValueInMgPerDl} mg/dL " +
+                PreviousMeasurementOverview = $"{previous!.ValueInMgPerDl} mg/dL " +
                     $"({currentMeasurement.ValueInMgPerDl - previous.ValueInMgPerDl:+#;-#;0})\n" +
                     $"{previous.UtcTimestamp.ToShortTimeString()}";
             }
 
-            FetchTime = $"@ {now.ToLocalTime().ToShortTimeString()}";
+            if (!hasChanged && !isFirst) {
+                return;
+            }
 
             // It seems they have a bug and the connection value is sooner than graph...
-            var measurements = graphResult.Data.GraphData.Append(graphResult.Data.Patient.GlucoseMeasurement);
+            var measurements = graphResult.Data.GraphData.Append(graphResult.Data.Patient.GlucoseMeasurement).ToList();
+            currentGraphData = measurements;
+
+            if (isFirst) {
+                SelectedTime = measurements[^1].UtcTimestamp.ToLocalTime().TimeOfDay;
+            }
 
             GraphData = new ISeries[] {
                 new LineSeries<GlucoseMeasurement>
                 {
-                    // For some reason the type DateTimePoint breaks the tooltips, workaround use a mapper.
-                    //Values = measurements.Select(m => new DateTimePoint(m.UtcTimestamp, m.ValueInMgPerDl)),
                     Values = measurements,
                     Stroke = new SolidColorPaint(new SKColor(33, 150, 242)) { StrokeThickness = 4 },
                     Fill = null,
                     GeometryFill = null,
                     GeometryStroke = null,
                     LineSmoothness = 0,
+                    // For some reason the type DateTimePoint breaks the tooltips, workaround use a mapper.
                     Mapping = (measurement, point) =>
                         point.Coordinate = new LiveChartsCore.Kernel.Coordinate(
                             measurement.UtcTimestamp.Ticks,
@@ -150,6 +256,7 @@ public partial class HomeViewModel : ObservableObject, IDisposable
             };
 
             GraphSections = new[] {
+                selectedDateRegion,
                 new RectangularSection {
                     Yi = patientInfo.TargetHigh,
                     Yj = patientInfo.TargetLow,
@@ -168,6 +275,9 @@ public partial class HomeViewModel : ObservableObject, IDisposable
                     Stroke = new SolidColorPaint(SKColors.Red) { StrokeThickness = 1 },
                 },
             };
+
+            FirstValueDateTime = measurements[0].UtcTimestamp;
+            LastValueDateTime = measurements[^1].UtcTimestamp;
         }
         catch (Exception ex)
         {
